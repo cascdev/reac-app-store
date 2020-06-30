f81cc1f364a00d6cdc813ca85cdf3b4a2829d632
cascdev <charles.ufrn@gmail.com>
2020-06-30 17:15:04 -0300

    Preparação do Template básico da Loja Virtual

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
-function App() {
+  
+const App = () =>  {
+
+  const ano = new Date().getFullYear()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <Fragment>
+    
+      <Header 
+        titulo='Loja Virtual React!'
+      />
 
-export default App;
+      <Footer 
+        ano={ano} 
+      /> 
+
+    </Fragment> 
+  )
+}
+ 
+  
+    
+
+
+
+export default App
+
---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = ({ano}) => ( 
+        <footer>
+                <h4>Todos os direitos reservados - Charlesasc &copy; | {ano}</h4>
+        </footer>
+)
+ 
+export default Footer
